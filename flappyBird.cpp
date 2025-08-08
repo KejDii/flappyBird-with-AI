b73758ac7323f5a200ccb7142dc1caec3829e784
KejDii <135364840+KejDii@users.noreply.github.com>
2025-08-08 14:00:04 +0200

    Update flappyBird.cpp with better nn class

--- flappyBird.cpp
+++ flappyBird.cpp
@@ -6,227 +6,15 @@
 #include <cstdlib>
 #include <vector>
 #include <array>
+#include "Neural_net.h"
 
 typedef std::vector<float> vector_1d;
 typedef std::vector<std::vector<float>> vector_2d;
 typedef std::vector<std::vector<std::vector<float>>> vector_3d;
 
-float sigmoid_derivative(float x) {
-	return x * (1 - x);
-}
-
-float sigmoid(float x) {
-	if (x > 5) {
-		return 1;
-	}
-	else if (x < -5) {
-		return 0;
-	}
-	else {
-		return 1 / (1 + exp(-x));
-	}
-}
-
-class Neuralnet {
-private:
-	vector_2d neural_network;
-	vector_3d weights;
-	vector_2d biases;
-	vector_2d errors;
-	vector_1d expected_outputs;
-	vector_1d inputs;
-	int INPUT_NEURONS;
-	int HIDDEN_NEURONS;
-	int OUTPUT_NEURONS;
-	int HIDDEN_LAYERS;
-	int LAYERS;
-	float LEARNING_RATE;
-public:
-	Neuralnet(int in_INPUT_NEURONS, int in_HIDDEN_NEURONS, int in_OUTPUT_NEURONS, int in_HIDDEN_LAYERS);
-	void set_inputs(vector_1d& ins);
-	void set_expoutputs(vector_1d& expout);
-	vector_1d forward_propagation();
-	void back_propagation();
-};
-
-Neuralnet::Neuralnet(int in_INPUT_NEURONS, int in_HIDDEN_NEURONS, int in_OUTPUT_NEURONS, int in_HIDDEN_LAYERS) {
-	std::mt19937 generator(rand() % 30);
-	std::uniform_real_distribution<float> value_distribution(-1.0, 1.0);
-
-	LEARNING_RATE = 0.01;
-	INPUT_NEURONS = in_INPUT_NEURONS;
-	HIDDEN_NEURONS = in_HIDDEN_NEURONS;
-	OUTPUT_NEURONS = in_OUTPUT_NEURONS;
-	HIDDEN_LAYERS = in_HIDDEN_LAYERS;
-	LAYERS = HIDDEN_LAYERS + 2;
-
-	neural_network.resize(LAYERS);
-	neural_network[0].resize(INPUT_NEURONS);
-	for (int i = 0; i < HIDDEN_LAYERS; i++) {
-		neural_network[i + 1].resize(HIDDEN_NEURONS);
-	}
-	neural_network[LAYERS - 1].resize(OUTPUT_NEURONS);
-
-	//weights resize
-	weights.resize((LAYERS - 1));
-	for (int i = 0; i < weights.size(); i++) {
-
-		weights[i].resize(neural_network[i + 1].size());
-
-		for (int j = 0; j < weights[i].size(); j++) {
-
-			weights[i][j].resize(neural_network[i].size());
-
-		}
-	}
-	for (int i = 0; i < weights.size(); i++) {
-
-		for (int j = 0; j < weights[i].size(); j++) {
-
-			for (int k = 0; k < weights[i][j].size(); k++) {
-
-				weights[i][j][k] = value_distribution(generator);
-
-			}
-		}
-	}
-
-	//biases resize
-	biases.resize(HIDDEN_LAYERS + 1);
-	for (int i = 0; i < HIDDEN_LAYERS; i++) {
-		biases[i].resize(HIDDEN_NEURONS);
-	}
-	biases[biases.size() - 1].resize(OUTPUT_NEURONS);
-
-	for (int i = 0; i < biases.size(); i++) {
-		for (int j = 0; j < biases[i].size(); j++) {
-			biases[i][j] = 0;
-		}
-	}
-
-	//errors resize
-	errors.resize(HIDDEN_LAYERS + 1);
-	for (int i = 0; i < HIDDEN_LAYERS; i++) {
-		errors[i].resize(HIDDEN_NEURONS);
-	}
-	errors[errors.size() - 1].resize(OUTPUT_NEURONS);
-
-	//other resize
-	inputs.resize(INPUT_NEURONS);
-	expected_outputs.resize(OUTPUT_NEURONS);
-	//END INIT
-}
-
-void Neuralnet::set_inputs(vector_1d& ins) {
-	inputs = ins;
-}
-
-void Neuralnet::set_expoutputs(vector_1d& expout) {
-	expected_outputs = expout;
-}
-
-vector_1d Neuralnet::forward_propagation() {
-	//inputs assignment
-	for (int i = 0; i < inputs.size(); i++) {
-		neural_network[0][i] = inputs[i];
-	}
-
-	//res neurons
-	for (int i = 0; i < neural_network.size() - 1; i++) {
-		for (int j = 0; j < neural_network[i + 1].size(); j++) {
-			neural_network[i + 1][j] = 0;
-		}
-	}
-
-	//forward propagation
-	for (int i = 0; i < weights.size(); i++) {
-
-		for (int j = 0; j < weights[i].size(); j++) {
-
-			for (int k = 0; k < weights[i][j].size(); k++) {
-
-				neural_network[1 + i][j] += neural_network[i][k] * weights[i][j][k];
-
-			}
-
-			neural_network[i + 1][j] += biases[i][j];
-
-
-			neural_network[1 + i][j] = sigmoid(neural_network[1 + i][j]);
-
-		}
-
-	}
 
 
 
-	//return outputs
-	return neural_network[neural_network.size() - 1];
-
-}
-
-void Neuralnet::back_propagation() {
-	//res errors
-	for (int i = 0; i < errors.size(); i++) {
-		for (int j = 0; j < errors[i].size(); j++) {
-			errors[i][j] = 0;
-		}
-	}
-
-
-	//1st layer of weights (closest to output)
-	for (int i = 0; i < weights[weights.size() - 1].size(); i++) {
-		for (int j = 0; j < weights[weights.size() - 1][i].size(); j++) {
-			weights[weights.size() - 1][i][j] -= LEARNING_RATE * 2 * (neural_network[neural_network.size() - 1][i] - expected_outputs[i]) * sigmoid_derivative(neural_network[neural_network.size() - 1][i]) * neural_network[neural_network.size() - 2][j];
-		}
-	}
-
-	//main errors
-	for (int i = 0; i < errors[errors.size() - 1].size(); i++) {
-		errors[errors.size() - 1][i] = pow(neural_network[neural_network.size() - 1][i] - expected_outputs[i], 1);
-	}
-
-	//errors in other layers
-	for (int i = 0; i < weights.size() - 1; i++) {
-
-		for (int j = 0; j < weights[weights.size() - 1 - i].size(); j++) {
-
-			for (int k = 0; k < errors[errors.size() - 2 - i].size(); k++) {
-
-				errors[errors.size() - 2 - i][k] += errors[errors.size() - 1 - i][j] * weights[weights.size() - 1 - i][j][k] * sigmoid_derivative(neural_network[neural_network.size() - 1 - i][j]);
-
-			}
-
-		}
-
-	}
-
-
-	//other layers weights alteration
-	for (int i = 0; i < weights.size() - 1; i++) {
-
-		for (int j = 0; j < weights[weights.size() - 2 - i].size(); j++) {
-
-			for (int k = 0; k < weights[weights.size() - 2 - i][j].size(); k++) {
-
-
-				weights[weights.size() - 2 - i][j][k] -= LEARNING_RATE * 2 * errors[weights.size() - 2 - i][j] * sigmoid_derivative(neural_network[weights.size() - 1 - i][j]) * neural_network[weights.size() - 2 - i][k];
-
-
-			}
-		}
-	}
-
-	//biases alteration
-	for (int i = 0; i < biases.size(); i++) {
-		for (int j = 0; j < biases[i].size(); j++) {
-
-			biases[i][j] -= LEARNING_RATE * 2 * errors[i][j];
-
-		}
-	}
-
-}
 
 class Bird {
 private:
@@ -535,7 +323,7 @@ class Game {
 private:
 	Bird player;
 	Pipes_Manager pipes;
-	Neuralnet* neural_network;
+	Neural_net* neural_network;
 	vector_2d inputs;
 	vector_2d outputs;
 	vector_2d expected_outputs;
@@ -560,14 +348,14 @@ private:
 	float record;
 	float act_rec;
 public:
-	Game(sf::RenderWindow& okno, Neuralnet& neural_net);
+	Game(sf::RenderWindow& okno, Neural_net& neural_net);
 	void play(sf::Event &event);
 	void point_counter(float pipes_x, float player_x);
 	void reset();
 	void backpropag();
 };
 
-Game::Game(sf::RenderWindow& okno, Neuralnet&neural_net) {
+Game::Game(sf::RenderWindow& okno, Neural_net&neural_net) {
 	window = new sf::RenderWindow;
 	window = &okno;
 	teksturatlo.loadFromFile("tlo.png");
@@ -678,7 +466,7 @@ void Game::play(sf::Event &event) {
 				//inputs[counter][4] = player.getVelocity();
 				//std::cout << "PLAYER Y - " << player.getPositiony() << " pipes gora " << pipes.pos_y(player.getPositionx()) << " pipes dol " << pipes.pos_y(player.getPositionx()) + 150 << " pipes x " << pipes.pos_x(player.getPositionx()) << "\n";
 				neural_network->set_inputs(inputs[counter]);
-				outputs[counter] = neural_network->forward_propagation();
+				outputs[counter] = neural_network->forward_propagation_out();
 				player.updateAI(outputs[counter][0]);
 				counter++;
 				if (counter == 10) {
@@ -857,9 +645,9 @@ int main() {
 
 	//INIT
 	int INPUT_NEURONS = 4;
-	int HIDDEN_NEURONS = 100;
+	int HIDDEN_NEURONS = 10;
 	int OUTPUT_NEURONS = 1;
-	int HIDDEN_LAYERS = 4;
+	int HIDDEN_LAYERS = 2;
 
 
 	vector_1d expected_outputs;
@@ -867,10 +655,11 @@ int main() {
 	vector_1d inputs;
 
 
-	Neuralnet neural_network(INPUT_NEURONS, HIDDEN_NEURONS, OUTPUT_NEURONS, HIDDEN_LAYERS);
+	Neural_net neural_network(INPUT_NEURONS, HIDDEN_NEURONS, OUTPUT_NEURONS, HIDDEN_LAYERS);
 	inputs.resize(INPUT_NEURONS);
 	outputs.resize(OUTPUT_NEURONS);
 	expected_outputs.resize(OUTPUT_NEURONS);
+	
 
 	sf::RenderWindow okno(sf::VideoMode(800, 600), "Flap Flap");
 	
